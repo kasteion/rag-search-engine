c923e7264192396d08548999bf985b128c8bc1bc
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2026-01-26 10:20:18 -0600

    CH9: Evaluation - Precision Metrics

---
+++ cli/evaluation_cli.py
@@ -0,0 +1,48 @@
+import argparse
+import json
+import os
+
+from lib.search_utils import GOLDEN_DATASET_PATH
+from lib.hybrid_search import rrf_search
+
+def main():
+    parser = argparse.ArgumentParser(description="Search Evaluation CLI")
+    parser.add_argument(
+        "--limit",
+        type=int,
+        default=5,
+        help="Number of results to evaluate (k for precision@k, recall@k)",
+    )
+
+    args = parser.parse_args()
+    limit = args.limit
+
+    # run evaluation logic here
+    with open(GOLDEN_DATASET_PATH, 'r') as f:
+        golden_dataset = json.load(f)
+
+    print(f"k={limit}")
+    
+    for test_case in golden_dataset['test_cases']:
+        query = test_case['query']
+        relevant_docs = test_case['relevant_docs']
+
+        print(f"- Query: {query}")
+
+        results, _ = rrf_search(query, 60, limit)
+
+        retrieved = []
+        relevant_retrieved = []
+        for r in results:
+            title = r['title']
+            retrieved.append(title)
+            if title in relevant_docs:
+                relevant_retrieved.append(title)
+        
+        precision = len(relevant_retrieved) / len(results)
+        print(f"  - Precision@{limit}: {precision:.4f}")
+        print(f"  - Retrieved: {', '.join(retrieved)}")
+        print(f"  - Relevant: {', '.join(relevant_retrieved)}")
+
+if __name__ == "__main__":
+    main()
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -9,6 +9,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
 STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
 CACHE_DIR = os.path.join(PROJECT_ROOT, "cache")
+GOLDEN_DATASET_PATH = os.path.join(PROJECT_ROOT, "data", "golden_dataset.json")
 
 BM25_K1 = 1.5
 BM25_B = 0.75
