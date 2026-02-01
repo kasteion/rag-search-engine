a782bbfbb133f7becb490f736d2c7f3478e24e1d
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2026-02-01 15:12:33 -0600

    CH10: Augmented Generation - LLM Summarization

--- cli/augmented_generation_cli.py
+++ cli/augmented_generation_cli.py
@@ -19,16 +19,21 @@ def main():
         "rag", help="Perform RAG (search + generate answer)"
     )
     rag_parser.add_argument("query", type=str, help="Search query for RAG")
+    rag_parser.add_argument("--limit", type=int, default=5, help="Results limit")
+
+    summarize_parser = subparsers.add_parser("summarize", help="Perform RAG (search + summarize)")
+    summarize_parser.add_argument("query", type=str, help="Search query for RAG")
+    summarize_parser.add_argument("--limit", type=int, default=5, help="Results limit")
 
     args = parser.parse_args()
+
+    client = genai.Client(api_key=api_key)
+    model = "gemini-2.5-flash"
 
     match args.command:
         case "rag":
             query = args.query
-            docs, _ = rrf_search(query, 60, 5)
-
-            client = genai.Client(api_key=api_key)
-            model = "gemini-2.5-flash"
+            docs, _ = rrf_search(query, 60, args.limit)
 
             prompt = f"""Answer the question or provide information based on the provided documents. This should be tailored to Hoopla users. Hoopla is a movie streaming service.
 
@@ -48,6 +53,28 @@ def main():
             print("\nRAG Response:")
             print(response.text)
 
+        case "summarize":
+            query = args.query
+            docs, _ = rrf_search(query, 60, args.limit)
+
+            prompt = f"""
+            Provide information useful to this query by synthesizing information from multiple search results in detail.
+            The goal is to provide comprehensive information so that users know what their options are.
+            Your response should be information-dense and concise, with several key pieces of information about the genre, plot, etc. of each movie.
+            This should be tailored to Hoopla users. Hoopla is a movie streaming service.
+            Query: {query}
+            Search Results:
+            {docs}
+            Provide a comprehensive 3–4 sentence answer that combines information from multiple sources:
+            """
+            response = client.models.generate_content(model=model, contents=prompt)
+
+            print("Search Results:")
+            for r in docs:
+                print(f" - {r['title']}")
+
+            print("\nLLM Summary:")
+            print(response.text)
         case _:
             parser.print_help()
 
